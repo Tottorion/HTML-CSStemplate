dafec179aec3bb82427f6989f9220126e23a9c7d
totton <u1_915da3@yahoo.co.jp>
2022-02-09 23:37:36 +0900

    レイアウト修正中

--- layout_demo.php
+++ layout_demo.php
@@ -13,31 +13,78 @@
     <?php include("header.php"); ?>
 
     <main class="wrapper layout_demo">
-        <section class="mv">
+
+        <section>
             <div class="content_wrap_100">
-                <div class="wrap_1clm">
-                    <div class="grid">
-                        <img src="https://placehold.jp/92aac7/ffffff/1200x600.png?text=MV%201200x600" alt="mv">
-                    </div>
-                </div>
-            </div>
-            <div class="content_wrap_100" style="background-color: #92aac7;">
-                <div class="wrap_1clm">
-                    <div class="grid">
+
+                <div class="font_size_40 bold text_align_ctr margin_top_50">幅100%</div>
+                <div class="wrap_clm">
+                    <div class="grid" style="background-color: #92aac7;">
                         <p style="color:white;">1カラム<br>
                             全幅</p>
                     </div>
                 </div>
 
-                <div class="wrap_2clm">
-                    <div class="grid30">
-                        <p style="color:white;">1カラム<br>
-                            全幅</p>
+
+                <div class="font_size_40 bold text_align_ctr margin_top_50">$inner_950</div>
+                <div class="inner_950">
+                    <div class="wrap_clm">
+                        <div class="grid" style="background-color: #92aac7;">
+                            <p style="color:white;">1カラム<br>
+                                全幅</p>
+                        </div>
                     </div>
-                    <div class="grid30">
-                        <p style="color:white;">1カラム<br>
-                            全幅</p>
+                </div>
+
+                <div class="font_size_40 bold text_align_ctr margin_top_50">2カラム幅同じ</div>
+                <div class="wrap_clm">
+                    <div class="grid50" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 50%</p>
                     </div>
+                    <div class="grid50 text_align_ctr" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 50%(text_align_ctr)</p>
+                    </div>
+                </div>
+
+
+                <div class="font_size_40 bold text_align_ctr margin_top_50">2カラム幅違い</div>
+                <div class="wrap_clm">
+                    <div class="grid30" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 30%</p>
+                    </div>
+                    <div class="grid50" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 50%</p>
+                    </div>
+                </div>
+
+
+                <div class="font_size_40 bold text_align_ctr margin_top_50">3カラム投稿</div>
+                <div class="wrap_clm post_clm">
+                    <div class="grid30" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 30%</p>
+                    </div>
+                    <div class="grid30" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 30%</p>
+                    </div>
+                    <div class="grid30" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 30%</p>
+                    </div>
+                    <div class="grid30" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 30%</p>
+                    </div>
+                    <div class="grid30" style="background-color: #92aac7;">
+                        <p style="color:white;">2カラム<br>
+                            幅 30%</p>
+                    </div>
+                    <div class="grid30"></div>
                 </div>
             </div>
         </section>
