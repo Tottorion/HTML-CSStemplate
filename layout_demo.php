9faf334520b73777112a880efd6e5120bb1370a5
totton <u1_915da3@yahoo.co.jp>
2022-02-11 18:15:12 +0900

    初版完成

--- layout_demo.php
+++ layout_demo.php
@@ -7,6 +7,27 @@
     <title>レイアウトDemo</title>
 
     <?php include("head.php"); ?>
+
+    <style>
+        .demo_p {
+            font-size: 20px;
+            text-align: center;
+            margin-top: -22px;
+            background-color: #fff;
+            width: fit-content;
+            padding: 0 8px;
+            margin-bottom: 30px;
+        }
+
+        .demo_p2 {
+            font-size: 20px;
+            text-align: center;
+            background-color: #fff;
+            width: fit-content;
+            padding: 0 8px;
+            margin: 10px auto;
+        }
+    </style>
 </head>
 
 <body>
@@ -15,8 +36,54 @@
     <main class="layout_demo">
 
         <section>
-            <div class="outer">
 
+            <div class="font_size_40 bold text_align_ctr margin_btm_50">各枠の設定</div>
+            <div class="full_width" style="border: solid 2px #666; background-color: #f7fdfd;">
+                <p class="demo_p">全幅枠</p>
+                <div class="outer" style="border: solid 2px #666; background-color: #edfcfc;">
+                    <p class="demo_p">outer枠</p>
+                    <div class="inner" style="border: solid 2px #666; background-color: #dcf8f8;">
+                        <p class="demo_p">inner枠</p>
+                        <div class="inner_950" style="border: solid 2px #666; background-color: #b8f1f1;">
+                            <p class="demo_p">inner1枠</p>
+                            <div class="inner_800" style="border: solid 2px #666; background-color: #95eaea;">
+                                <p class="demo_p">inner2枠</p>
+                                <div class="inner_600" style="border: solid 2px #666; background-color: #72e2e2;">
+                                    <p class="demo_p">inner3枠</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="font_size_40 bold text_align_ctr margin_btm_30 margin_top_30">レスポンシブ確認用</div>
+
+            <div class="full_width" style="border: solid 2px #666; background-color: #f7fdfd;">
+                <p class="demo_p2">全幅枠</p>
+            </div>
+            <div class="outer" style="border: solid 2px #666; background-color: #edfcfc;">
+                <p class="demo_p2">outer枠</p>
+            </div>
+            <div class="inner" style="border: solid 2px #666; background-color: #dcf8f8;">
+                <p class="demo_p2">inner枠</p>
+            </div>
+            <div class="inner_950" style="border: solid 2px #666; background-color: #b8f1f1;">
+                <p class="demo_p2">inner1枠</p>
+            </div>
+            <div class="inner_800" style="border: solid 2px #666; background-color: #95eaea;">
+                <p class="demo_p2">inner2枠</p>
+            </div>
+            <div class="inner_600" style="border: solid 2px #666; background-color: #72e2e2;">
+                <p class="demo_p2">inner3枠</p>
+            </div>
+
+
+
+
+
+            <div class="outer">
                 <div class="font_size_40 bold text_align_ctr margin_top_50">幅100%</div>
                 <div class="wrap_clm">
                     <div class="grid" style="background-color: #92aac7;">
@@ -24,7 +91,6 @@
                             全幅</p>
                     </div>
                 </div>
-
 
                 <div class="font_size_40 bold text_align_ctr margin_top_50">$inner_950</div>
                 <div class="inner_950">
@@ -103,21 +169,21 @@
 
                 <div class="font_size_40 bold text_align_ctr margin_top_50">4カラム</div>
                 <div class="wrap_4clm">
-                    <div class="grid20" style="background-color: #92aac7;">
+                    <div class="grid225" style="background-color: #92aac7;">
                         <p style="color:white;">2カラム1<br>
-                            幅 20%</p>
+                            幅 22.5%</p>
                     </div>
-                    <div class="grid20" style="background-color: #92aac7;">
+                    <div class="grid225" style="background-color: #92aac7;">
                         <p style="color:white;">2カラム2<br>
-                            幅 20%</p>
+                            幅 22.5%</p>
                     </div>
-                    <div class="grid20" style="background-color: #92aac7;">
+                    <div class="grid225" style="background-color: #92aac7;">
                         <p style="color:white;">2カラム3<br>
-                            幅 20%</p>
+                            幅 22.5%</p>
                     </div>
-                    <div class="grid20" style="background-color: #92aac7;">
+                    <div class="grid225" style="background-color: #92aac7;">
                         <p style="color:white;">2カラム4<br>
-                            幅 20%</p>
+                            幅 22.5%</p>
                     </div>
 
                 </div>
